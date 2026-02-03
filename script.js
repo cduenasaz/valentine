2820424353bc928788b78c0fcb1608d80855dbcf
cduenasaz <cduenasaz@arizona.edu>
2026-02-02 18:11:13 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,31 +2,53 @@ const yes = document.getElementById("yes");
 const no = document.getElementById("no");
 const subtext = document.getElementById("subtext");
 var counter = 0;
+var counter2 = 0;
+var sizeLimit = 7;
 var moveLimit = 6;
+let yesSize = 16;
 let messages = [
   "are we deadahh",
-  "lol jokes over nigga",
-  "accidents happen, 2nd chance princess!",
+  "lol jokes over",
+  "accidents happen, 2nd chance",
   "stop hurting my feelings",
   "reservation to north",
   "ive spent my life savings on u",
   "im getting cheated on",
   "IM GETTING CHEATED ON??",
   "now ive lost my appetite and all will to live",
-  "IM GONNA RIP DIOR TO SHREDS"
+  "IM GONNA RIP DIOR TO SHREDS",
+  "at some point you gotta say yes",
+  "do u know how long this took to make",
+  "please",
+  "yo this cant be lola",
+  "we legit play uno tg",
+  "no",
+  "im gonna tell ur mom",
+  "im gonna tell max",
+  "im gonna tell cece",
+  "ill buy u dutch",
+  "food?"
+  
+  
 ];
 
 let index = 0;
 
 no.addEventListener("click", () => {
   let question = document.getElementById("question");
-
+  
   question.innerText = messages[index];
 
   index++;
-
+  counter2++;
   if (index >= messages.length) {
     index = messages.length - 1;
+  }
+  
+  if(counter2 >= sizeLimit){
+    yesSize += 8; // how much it grows each time
+    yes.style.fontSize = yesSize + "px";
+    yes.style.padding = (yesSize/2) + "px " + (yesSize) + "px";
   }
 });
 
